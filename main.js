400d9abe5027c9e34e2c90fd91affd2d62dc0e9a
WhenPigsFly12 <99145987+WhenPigsFly12@users.noreply.github.com>
2022-11-10 18:25:40 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+
+function start() {
+    document.getElementById("textbox").innerHTML = "";
+    recognition.start();
+}
+
+recognition.onresult = function(event) {
+    console.log(event);
+    var content = event.results[0][0].transcript;
+    console.log(content);
+    document.getElementById("textbox").innerHTML = content
+    if (content == "take my selfie") {
+        speak();
+    }
+}
+
+function speak() {
+    var synth = window.speechSynthesis;
+    var speak_data = "Taking Your Selfie In 5 Seconds"
+    var utterthis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterthis);
+    Webcam.attach(Camera);
+    setTimeout(function(){
+        take_snapshot();
+        save();
+    },5000)
+}
+
+
+Webcam.set({
+    width: 360,
+    height: 250,
+    image_format: "png",
+    png_quality: 90
+});
+
+Camera = document.getElementById("camera");
+
+function take_snapshot() {
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = "<img id='selfie' src='" + data_uri + "'>";
+    })
+}
+
+function save() {
+    link = document.getElementById("link");
+    img = document.getElementById("selfie");
+    link.href = img;
+    link.click();
+}
